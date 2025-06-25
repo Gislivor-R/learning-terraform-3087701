f64b09b48bfe46e7a9438f918e2f00d8f91ca9e9
Gislivor-R <raul.ballesteros.gtz@gmail.com>
2025-06-25 15:56:36 -0600

    Update variables.tf uncommented

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
-#variable "project" {
-#  description = "GCP project ID"
-#  type        = string
-#}
-#
-#variable "region" {
-#  description = "GCP region"
-#  type        = string
-#  default     = "us-central1"
-#}
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
 
-#variable "zone" {
-#  description = "GCP zone"
-#  type        = string
-#  default     = "us-central1-c"
-#}
-#
-#variable "google_credentials" {
-#  description = "Service account credentials in JSON"
-#  type        = string
-#  sensitive   = true
-#}
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-c"
+}
+
+variable "google_credentials" {
+  description = "Service account credentials in JSON"
+  type        = string
+  sensitive   = true
+}
