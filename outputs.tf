af72270b8e7efadae9dea92ef04fd837d78a2afd
Gislivor-R <raul.ballesteros.gtz@gmail.com>
2025-06-25 15:56:56 -0600

    Update outputs.tf uncommented

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-#output "instance_name" {
-#  value = google_compute_instance.web.name
-#}
-#
-#output "instance_zone" {
-#  value = google_compute_instance.web.zone
-#}
+output "instance_name" {
+  value = google_compute_instance.web.name
+}
+
+output "instance_zone" {
+  value = google_compute_instance.web.zone
+}
 
