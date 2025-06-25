8db9c1d7727056370bc7047b637d0234fa93c05b
Gislivor-R <raul.ballesteros.gtz@gmail.com>
2025-06-25 15:44:26 -0600

    Update providers.tf to GCP resources

--- providers.tf
+++ providers.tf
@@ -1,11 +1,14 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+    google = {
+      source  = "hashicorp/google"
     }
   }
 }
 
-provider "aws" {
-  region  = "us-west-2"
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+  credentials = var.google_credentials
 }
